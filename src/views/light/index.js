09cf2d52cea258e71d6522ca15abf2ff93553fcd
erekli <erekli@tencent.com>
2021-05-26 17:30:30 +0800

    add灯光和材质

--- src/views/light/index.js
+++ src/views/light/index.js
@@ -1,6 +1,7 @@
 import './index.scss'
 import * as THREE from 'three'
 import { useCallback, useEffect,useRef} from 'react'
+import { LightShadow } from 'three'
 
 const Page =() =>{
     const Body = useRef()
@@ -10,6 +11,7 @@ const Page =() =>{
     const Camera = useRef(new THREE.PerspectiveCamera()).current //透视相机
     const Render = useRef(new THREE.WebGL1Renderer({ antialias: true})).current  // 渲染器
     const Meshs = useRef([]).current
+    const Lights = useRef([]).current
     const id = useRef(null)
 
     // 初始化
@@ -30,7 +32,37 @@ const Page =() =>{
         Camera.lookAt(0,0,0)
         // 任何参数发生改变都要被调用
         Camera.updateProjectionMatrix()
+        // 设置场景背景为白色
+        // Render.setClearColor(0xffffff)
     },[Render, Body])
+
+    // 渲染画面
+    const renderScene = useCallback(()=>{
+        Render.render(Scene, Camera)
+        Meshs.forEach(item=>{
+            item.rotation.x += 0.5/180*Math.PI
+            item.rotation.y += 0.5/180*Math.PI
+        })
+        id.current=window.requestAnimationFrame(()=>renderScene())
+    },[Render])
+
+    // 新建灯光
+    const createLight = useCallback(()=>{
+        // // 太阳光
+        // const dirLight = new THREE.DirectionalLight('#ffffff',0.5)
+        // // 从右上角斜的方向照下来
+        // dirLight.position.set(100,100,100)
+
+        // 点光源 8是说灯光照到距离8
+        const point = new THREE.PointLight('#ffffff',2,8)
+        point.position.set(0,5,0)
+
+        // 环境光 是四面八方 所以不需要设置位置
+        const ambLight = new THREE.AmbientLight('#ffffff',0.3)
+
+        Scene.add(point, ambLight)
+        Lights.push(point, ambLight)
+    },[])
 
     // 新建一个立方体
     const createRect = useCallback(()=>{
@@ -45,38 +77,87 @@ const Page =() =>{
         Meshs.push(mesh)
     },[])
 
-    // 每一帧渲染画面的方法
-    const renderScene = useCallback(()=>{
-        Render.render(Scene, Camera)
-        Meshs.forEach(item=>{
-            // item.rotation.x += 0.5/180*Math.PI
-            item.rotation.y += 0.5/180*Math.PI
-        })
-        id.current=window.requestAnimationFrame(()=>renderScene())
-    },[Render])
+    
+    // 创建线条
+    const createLine = useCallback(()=>{
+        // 创建基础线条材质vertexColors是每个顶点指定一种颜色
+        const lineMater = new THREE.LineBasicMaterial({vertexColors: true})
+        // 创建几何体
+        const geometry = new THREE.BufferGeometry()
+        // 新建一个数组当(x,y,z)三元组
+        const positions=[]
+        // 新建一个数组当color三元组随机生成
+        const colors =[]
+        // 定义正方体半径
+        const r=2
+        for (let i=0;i<5000;i++){
+            // 在canvas里中心点在左上角，x轴向右，y轴向下
+            // 在threejs里因为立方体的中心点实际上在立方块中心，所以这里是-1~1范围内
+            const x = Math.random()*r - r/2
+            const y = Math.random()*r - r/2
+            const z = Math.random()*r - r/2
+            positions.push(x,y,z)
+            colors.push( ( x / r ) + 0.5 );
+			colors.push( ( y / r ) + 0.5 );
+			colors.push( ( z / r ) + 0.5 );
+        }
+        geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ) );
+		geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
+        // 第一个对象为网格格点对象
+        const mesh = new THREE.Line(geometry,lineMater)
+        mesh.position.set(4,0,0)
+        Scene.add(mesh)
+        Meshs.push(mesh)
+    },[])
 
-    const createLine =
+    // 创建lambert材质立方体
+    const createLambert =useCallback(()=>{
+        const lambert = new THREE.MeshLambertMaterial({color:'red'})
+        const rect = new THREE.BoxBufferGeometry(2,2,2)
+        const mesh = new THREE.Mesh(rect, lambert)
+        mesh.position.set(-4,0,0)
+        Scene.add(mesh)
+        Meshs.push(mesh)
+    },[])
 
-    // 
+    // 创建phong材质立方体 网格材质 具有镜面高光
+    const createPhong =useCallback(()=>{
+        const phong = new THREE.MeshPhongMaterial({color:'red'})
+        const rect = new THREE.BoxBufferGeometry(2,2,2)
+        const mesh = new THREE.Mesh(rect, phong)
+        mesh.position.set(-8,0,0)
+        Scene.add(mesh)
+        Meshs.push(mesh)
+    },[])
+
     useEffect(()=>{
         console.log(1)
         // 把渲染器加到Body的DOM节点里
         Body.current.append(Render.domElement)
         init()
+        createLight()
         createRect()
+        createLine()
+        createLambert()
+        createPhong()
         renderScene()
+        console.log(1)
         // 销毁钩子
-        return()=>{
+        return ()=>{
             cancelAnimationFrame(id.current)
-            // 从场景中删除
+            // 物体把自己从场景中删除
             Meshs.forEach(item=>{
-                Scene.remove()
+                Scene.remove(item)
                 item.geometry.dispose()
                 item.material.dispose()
             })
+            // 移除光线
+            Lights.forEach(item=>{
+                Scene.remove(item)
+            })
+            // 移除渲染器
             Render.dispose()
             Scene.dispose()
-
         }
     },[])
 
